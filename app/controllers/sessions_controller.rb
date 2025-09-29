547799dc1c0db984b389aa0e4553799135ecd303
Paul Nunez <v-paul.nunez@pinger.com>
2025-09-29 12:16:36 +0200

    add sessions react form

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,14 +3,19 @@ class SessionsController < ApplicationController
   rate_limit to: 10, within: 3.minutes, only: :create, with: -> { redirect_to new_session_url, alert: "Try again later." }
 
   def new
+    render inertia: "Session/New", props: {}
   end
 
   def create
-    if user = User.authenticate_by(params.permit(:email_address, :password))
+    user = User.authenticate_by(params.permit(:email_address, :password))
+
+    if user
       start_new_session_for user
       redirect_to after_authentication_url
     else
-      redirect_to new_session_path, alert: "Try another email address or password."
+      # redirect_to new_session_path, alert: "Try another email address or password."
+      puts "REDIRECTING PAUL"
+      redirect_to new_session_path, inertia: { errors: { login: "Try another email address or password." } }
     end
   end
 
